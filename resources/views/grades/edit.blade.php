dd18a8bf8a0360c369ecbf22d26aae775ada3237
Lucvanwesten2003 <70520279+Lucvanwesten2003@users.noreply.github.com>
2021-05-31 13:32:14 +0200

    added errors to edit grades

--- resources/views/grades/edit.blade.php
+++ resources/views/grades/edit.blade.php
@@ -15,12 +15,24 @@
                 @method('PUT')
                 <label for="blok">Blok:</label><br>
                 <input type="text" id="blok" name="blok" value=" {{ $grade->blok  }}"><br>
+                @error('blok')
+                <p style="color: red;">{{ $errors->first('blok') }}</p>
+                @enderror
                 <label for="course_name">Course name:</label><br>
                 <input type="text" id="course_name" name="course_name" value=" {{ $grade->course_name }}"><br>
+                @error('course_name')
+                <p style="color: red;">{{ $errors->first('course_name') }}</p>
+                @enderror
                 <label for="EC">EC:</label><br>
                 <input type="text" id="EC" name="EC" value=" {{ $grade->EC }}"><br>
+                @error('EC')
+                <p style="color: red;">{{ $errors->first('EC') }}</p>
+                @enderror
                 <label for="best_grade">Best grade:</label><br>
                 <input type="text" id="best_grade" name="best_grade" value=" {{ $grade->best_grade }}"><br><br>
+                @error('best_grade')
+                <p style="color: red;">{{ $errors->first('best_grade') }}</p>
+                @enderror
                 <input type="submit" value="Submit">
             </form>
         </div>
